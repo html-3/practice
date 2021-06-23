906de825daea47472f38ac14ee11a0d82ae2da35
html-3 <henriquemarques@ufrj.br>
2021-06-23 15:31:45 -0300

    Pratice 2

--- BinaryTree/function.py
+++ BinaryTree/function.py
@@ -1,4 +1,41 @@
-def ReverseString(string):
-  # by slicing or stepping backwards the string is reversed.
-  # [index start: index end: step]
-  return string[::-1]
+def BinaryTree(listInput):
+  # (childNode, parentNode)
+  # every child node is related to one parent,
+  # every parent nodes can have at most two children,
+  # and there is a parentNode who's a root
+
+  
+  # parsing the list of strings of pairs to
+  # a list of strings of ids
+  listNodes = []
+  dictNodes = {}
+  for i in listInput:
+    listNodes += i[1:-1].split(',')
+
+  
+  length = len(listNodes)
+
+  # parsing the list of strings of ids to
+  # a dictionary of keys children ids and values parent ids
+  j = 0
+  while length > j:
+    # defining parent and child nodes, they come in pairs, so...
+    childNode = listNodes[j]
+    parentNode = listNodes[j + 1]
+
+    # one child node has two parents
+    if dictNodes.get(childNode):
+      return "false"
+    dictNodes[childNode] = parentNode
+
+    j += 2
+
+  # list of parent nodes
+  parentNodes = list(dictNodes.values())
+
+  for k in parentNodes:
+    # parent node with more than 2 children
+    if parentNodes.count(k) > 2:
+      return "false"
+  
+  return "true"
--- BinaryTree/testset.py
+++ BinaryTree/testset.py
@@ -1,13 +1,28 @@
-from function import ReverseString
+from function import BinaryTree
 
 # hypothetical list to test the function.
 testset = [
-            "Hello World",
-            "Coderbyte",
-            "12345",
-            "above\nbelow"
+            ["(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)"],
+            ["(1,2)", "(2,4)", "(7,2)"], 
+            ["(1,2)", "(3,2)", "(2,12)", "(5,2)"],
+            ["(2,5)", "(2,6)"],
+            ["(2,3)", "(1,2)", "(4,9)", "(9,3)", "(12,9)", "(6,4)"],
+            ["(5,6)", "(6,3)", "(2,3)", "(12,5)"],
+            ["(10,20)"],
+            ["(2,3)", "(1,2)", "(4,9)", "(9,3)", "(12,9)", "(6,4)", "(1,9)"],
+            ["(10,20)", "(20,50)"]
           ]
-          
+
+          # true
+          # true
+          # false
+          # false
+          # true
+          # true
+          # true
+          # false
+          # true
+
 # testing every string in the list
 for i in testset:
-    print(ReverseString(i))
+    print(BinaryTree(i))
